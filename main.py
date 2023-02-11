5f5bc43af8abf1b0fdd63171a60bf97c4bc4e8f5
roham <rohamshahedi94@gmail.com>
2023-02-11 21:39:20 +0330

    add random point

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import csv
+import random
+
 with open('list.csv') as f:
     reader = csv.reader(f, delimiter=',')
     for row in reader:
-        print("Name: {}   Number: {}".format(row[0], row[1]))
+        print("Name: {}   Number: {}".format(row[0],random.randint(1,70)))
